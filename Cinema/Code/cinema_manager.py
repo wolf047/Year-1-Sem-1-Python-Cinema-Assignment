4373ab83125f2d74192b42cd707dd3ff8dfc9ebd
wolf047 <enxin06@gmail.com>
2025-08-26 08:37:18 +0800

    Cinema: add Code and Database files

---
+++ Cinema/Code/cinema_manager.py
@@ -0,0 +1,17 @@
+# Cinema Manager
+# - Add, update, or remove movie listings.
+# - Create and manage showtime schedules(movie, auditorium, time).
+# - Set ticket prices and apply discount policies.
+# - View overall booking reports and revenue summaries.
+
+def add_movie_listing():
+
+def update_movie_listing():
+
+def remove_movie_listing():
+
+def create_showtime():
+
+def update_showtime():
+
+def 
